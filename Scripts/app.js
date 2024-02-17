6038fd897fe52a930a7f03668482bba9d526966e
WarDudeFoxes <153716884+WarDudeFoxes@users.noreply.github.com>
2024-02-17 08:02:27 -0800

    Add files via upload

---
+++ Scripts/app.js
@@ -0,0 +1,79 @@
+import { sendEmail } from "./contact.js";
+
+
+const popElem = document.querySelector('.pop-ul')
+const menuTxt = document.querySelector('.menu-text') 
+
+document.querySelectorAll('.menu, .menu-text').forEach(elem => {
+  elem.addEventListener('click', () => {
+    popElem.classList.toggle('show')
+    if (menuTxt.innerText === 'Menu') {
+      menuTxt.innerText = 'Close'
+    } else {
+      menuTxt.innerText = 'Menu'
+    }
+  });
+});
+
+document.querySelectorAll('.home').forEach(elem => {
+  elem.addEventListener('click', () => {
+  location.reload()
+  });
+});
+
+document.querySelectorAll('.about').forEach(elem => {
+  elem.addEventListener('click', () => {
+    document.querySelector('.about-cont').scrollIntoView()
+    popElem.classList.remove('show')
+  });
+});
+
+document.querySelectorAll('.projects').forEach(elem => {
+  elem.addEventListener('click', () => {
+    document.querySelector('.projects-cont').scrollIntoView()
+    popElem.classList.remove('show')
+  });
+});
+
+document.querySelectorAll('.technologies').forEach(elem => {
+  elem.addEventListener('click', () => {
+    document.querySelector('.tecnologies-cont').scrollIntoView()
+    popElem.classList.remove('show')
+  });
+});
+
+document.querySelectorAll('.contact').forEach(elem => {
+  elem.addEventListener('click', () => {
+    document.querySelector('.contact-cont').scrollIntoView();
+  });
+});
+
+document.querySelectorAll('ul, .left-section, .contact-cont, .tecnologies-cont, .projects-cont, .about-cont, footer').forEach(elem => {
+  elem.addEventListener('click', () => {
+    popElem.classList.remove('show');
+    menuTxt.innerText = 'Menu'
+  });
+});
+
+window.addEventListener('scroll', () => {
+  popElem.classList.remove('show');
+  menuTxt.innerText = 'Menu';
+});
+
+document.querySelector('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  let name = document.querySelector('.name').value
+  let email = document.querySelector('.email').value
+  let message = document.querySelector('.message').value
+  let dataObj = {
+    title: 'Message From Porfolio',
+    name,
+    email,
+    message
+  }
+
+  sendEmail(dataObj);
+  document.querySelector('.name').value = ''
+  document.querySelector('.email').value = ''
+  document.querySelector('.message').value = ''
+})
